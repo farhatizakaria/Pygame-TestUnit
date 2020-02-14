0129ea090a6090493c7e78073adc26c63fccb98b
= <zakariafarhati18@gmail.com>
2020-02-14 20:14:22 +0000

    Organizing the script

--- hulo.py
+++ hulo.py
@@ -1,39 +1,43 @@
 import pygame
-pygame.init()
-
-win = pygame.display.set_mode((500, 500))
-
-pygame.display.set_caption("My first game")
-
-x = 50
-y = 50
-width = 40
-height = 40
-vel = 10
 
 
-run = True
-while run:
-    pygame.time.delay(100)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
 
-    keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_LEFT]:
-        x -= vel
-    if keys[pygame.K_RIGHT]:
-        x += vel
-    if keys[pygame.K_UP]:
-        y -= vel
-    if keys[pygame.K_DOWN]:
-        y += vel
+def setup():
+    """ The setup method for display and set mode, caption ..."""
+    pygame.init()
+    #win = pygame.display.set_mode((500, 500))
+    pygame.display.set_caption("Hulo Game")
     
+    run()
 
-    pygame.draw.rect(win, (255, 255, 0), (x, y, width, height))
-    pygame.display.update()
+def run():
+    """ The main running function """
+    win = pygame.display.set_mode((500, 500))
+    x = 50
+    y = 50
+    width = 40
+    height = 40
+    vel = 15 # Velocity
+    run = True
+    while  run:
+        pygame.time.delay(100)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            x -= vel
+        if keys[pygame.K_RIGHT]:
+            x += vel
+        if keys[pygame.K_UP]:
+            y -= vel
+        if keys[pygame.K_DOWN]:
+            y += vel
 
-
-pygame.quit()
+        pygame.draw.rect(win, (255, 255, 0), (x, y, width, height))
+        pygame.display.update()
+    
+    pygame.quit()
+if __name__ == "__main__":
+    setup()
